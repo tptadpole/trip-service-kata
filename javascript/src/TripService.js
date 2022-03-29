155c49e8f61a2c909796cda2b1768fd7d8b1bc77
max.lee <f820602h@gmail.com>
2022-03-29 16:55:39 +0800

    完善 class user

--- javascript/src/TripService.js
+++ javascript/src/TripService.js
@@ -1,30 +1,15 @@
 "use strict";
 
-let UserSession = require('./UserSession');
-let TripDAO = require('./TripDAO');
+let UserSession = require("./UserSession");
+let TripDAO = require("./TripDAO");
 
 class TripService {
-    getTripsByUser(user) {
-        let tripList = [];
-        let loggedUser = UserSession.getLoggedUser();
-        let isFriend = false;
-        if (loggedUser != null) {
-            let friends = user.getFriends();
-            for (let i=0; i < friends.length; i++) {
-                let friend = friends[i];
-                if (friend == loggedUser) {
-                    isFriend = true;
-                    break;
-                }
-            };
-            if (isFriend) {
-                tripList = TripDAO.findTripsByUser(user);
-            }
-            return tripList;
-        } else {
-            throw new Error('User not logged in.');
-        }
-    }
+  getTripsByUser(user) {
+    let loggedUser = UserSession.getLoggedUser();
+    if (!loggedUser) throw new Error("User not logged in.");
+
+    return user.isFriendsWith(loggedUser) ? [] : TripDAO.findTripsByUser(user);
+  }
 }
 
-module.exports = TripService
+module.exports = TripService;
--- javascript/src/User.js
+++ javascript/src/User.js
@@ -1,5 +1,28 @@
 "use strict";
 
 module.exports = class User {
+  constructor() {
+    this.trips = [];
+    this.friends = [];
+  }
 
-}
+  getFriends() {
+    return this.friends;
+  }
+
+  addFriend(user) {
+    this.friends.push(user);
+  }
+
+  getTrips() {
+    return this.trips;
+  }
+
+  addTrip(trip) {
+    this.trips.push(trip);
+  }
+
+  isFriendsWith(user) {
+    return this.friends.some((friend) => friend === user);
+  }
+};
