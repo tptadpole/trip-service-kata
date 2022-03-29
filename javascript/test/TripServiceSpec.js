04127f75fa813bdaa1298a0c2c9a7dc5bec49cb0
max.lee <f820602h@gmail.com>
2022-03-29 17:14:57 +0800

    列出測試項目

--- javascript/test/TripServiceSpec.js
+++ javascript/test/TripServiceSpec.js
@@ -1,12 +1,18 @@
 "use strict";
 
-let assert = require('assert');
-let TripService = require('../src/TripService');
+let assert = require("assert");
+let TripService = require("../src/TripService");
 
-describe('TripService', () => {
+describe("TripService", () => {
+  xit("should_throw_error_when_user_us_not_logged_in", () => {
+    //
+  });
 
-    it('should... ', () => {
-        assert.equal(2+2, 5);
-    });
+  xit("should_not_return_tip_when_users_are_not_friend", () => {
+    //
+  });
 
+  xit("should_return_tip_when_users_are_friend", () => {
+    //
+  });
 });
