19f9fc6a6754a3204c73208640a398611d12776d
max.lee <f820602h@gmail.com>
2022-03-29 17:25:29 +0800

    完成 should_throw_error_when_user_us_not_logged_in

--- javascript/test/TripServiceSpec.js
+++ javascript/test/TripServiceSpec.js
@@ -1,11 +1,15 @@
 "use strict";
 
-let assert = require("assert");
-let TripService = require("../src/TripService");
+const assert = require("assert");
+const TripService = require("../src/TripService");
+const MockTripDAO = require("./MockTripDAO");
+const NotLoggedException = require("../src/NotLoggedException");
 
 describe("TripService", () => {
-  xit("should_throw_error_when_user_us_not_logged_in", () => {
-    //
+  it("should_throw_error_when_user_us_not_logged_in", () => {
+    const tripService = new TripService(MockTripDAO);
+
+    assert.throws(tripService.getTripsByUser, NotLoggedException);
   });
 
   xit("should_not_return_tip_when_users_are_not_friend", () => {
