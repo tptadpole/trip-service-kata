af26afdd0e22e2be6ef96c306ef43eb823637fef
tptadpole <wk7293@gmail.com>
2022-04-21 15:22:42 +0800

    完成第二個測試

--- php/test/Test/TripServiceKata/Trip/TripServiceTest.php
+++ php/test/Test/TripServiceKata/Trip/TripServiceTest.php
@@ -4,6 +4,7 @@ namespace Test\TripServiceKata\Trip;
 
 use PHPUnit\Framework\TestCase;
 use TripServiceKata\Exception\UserNotLoggedInException;
+use TripServiceKata\Trip\Trip;
 use TripServiceKata\Trip\TripService;
 use TripServiceKata\User\User;
 use TripServiceKata\User\UserSession;
@@ -35,6 +36,19 @@ class TripServiceTest extends TestCase
 
     public function testShouldNotReturnTripsWhenLoggedUserAreNotFriend()
     {
+        $mockUserSession = $this->createMock(UserSessionHelper::class);
+        $mockUserSession->method('getLoggedUser')
+            ->willReturn(new User('b'));
+
+        $aUser = new User('a');
+        $aUser->addFriend(new User('c'));
+        $aUser->addTrip(new Trip());
+
+        // Act
+        $actual = $this->target->getTripsByUser($aUser, $mockUserSession);
+
+        // Assert
+        $this->assertSame([], $actual);
     }
 
     public function testShouldReturnTripsWhenLoggedUserAreFriend()
